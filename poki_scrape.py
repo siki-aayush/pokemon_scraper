5c26f4a2054b9d8d54fe39d39392ccc73ee7e30a
siki <musyajuaayush@gmail.com>
2020-05-18 23:17:43 +0545

    pokedex scraping

---
+++ poki_scrape.py
@@ -0,0 +1,39 @@
+import requests
+from bs4 import BeautifulSoup
+import csv
+
+titles = ['name', 'description', 'height', 'weight', 'gender', 'category', 'abilities', 'type', 'weakness']
+with open('pokedex.csv', 'w') as f:
+    csv_writer = csv.writer(f, delimiter=',')
+    csv_writer.writerow(titles)
+    base ='https://www.pokemon.com' 
+    url = 'https://www.pokemon.com/us/pokedex/bulbasaur'
+    while True:
+        rows = []
+
+        response = requests.get(url)
+        soup = BeautifulSoup(response.text, 'html.parser')
+        divs = soup.find('div', 'pokedex-pokemon-pagination-title')
+        rows.append(divs.div.text.split()[0])
+        
+        description = soup.find('p', 'version-x')
+        rows.append(description.text.strip())
+        
+        attributes = soup.find_all('span', 'attribute-title')
+        for el in attributes:
+            rows.append(el.parent.find('span', 'attribute-value').string)
+        
+        types_el = soup.find('div', 'dtm-type')
+        rows.append(','.join([temp.text for temp in types_el.find_all('a')]))
+        
+        weaknesses_el = soup.find('div', 'dtm-weaknesses')
+        rows.append(','.join([temp.text.strip() for temp in weaknesses_el.find_all('a')]))
+        
+        csv_writer.writerow(rows)
+        next_url = soup.find('a', 'next')
+        url = base + next_url['href']
+        
+        if url == 'https://www.pokemon.com/us/pokedex/bulbasaur':
+            break
+        print(f'-->{rows[0]}')
+        
